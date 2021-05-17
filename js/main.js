cc76de89366df830a46d097d75ae788e1b122c7f
Armin Dzibric <armindzibric6@gmail.com>
2021-05-17 23:23:37 +0200

    Setup fetch API - stage 1

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let movie;
 let apiUrl = "https://api.themoviedb.org/3/";
 let type;
 let apiKey = "?api_key=8c1a9592c5e5592b4ae3683c1656a158";
-
+let genreName = null;
 
 
 
@@ -11,6 +11,7 @@ function getTop10Movies() {
     const NUMBER_OF_TOP_MOVIES = 7;
     type = "movie/top_rated";
     let page = "&page_1";
+    let imagePathPrefix = "https://www.themoviedb.org/t/p/w500";
     let url = apiUrl + type + apiKey + page;
 
     fetch(url).then(function(response) {
@@ -20,28 +21,31 @@ function getTop10Movies() {
         for (let i = 0; i <= NUMBER_OF_TOP_MOVIES; i++) {
             let title = obj.results[i].title;
             let genre = obj.results[i].genre_ids[0];
+            let img = imagePathPrefix + obj.results[i].poster_path;
+            let year = obj.results[i].release_date.substring(0, 4);
+            console.log(obj.results);
 
 
             if (i <= NUMBER_OF_TOP_MOVIES / 2) {
                 document.getElementById("topEightFirstPage").innerHTML += "<div class=' card mx-1 my-4 ' style='max-width: 12rem; max-height:22rem;'>" +
-                    "<img class='card-img-top mx-auto my-auto' style='max-width:10rem; max-height:12rem;' src='img/intothestorm.jpg' alt='Card image cap'>" +
-                    "<div class='card-body'>" +
-                    " <a href='#' class='text-decoration-none text-dark'>" +
-                    "  <h5 class='card-title' id='title'>" + title + "</h5>" +
-                    " </a>" +
-                    "  <a href='#' id='genre' class='card-link text-muted text-decoration-none'>Genre</a>" +
-                    " <a href='#' id='year' class='card-link text-muted text-decoration-none'>(2020)</a>" +
-                    " </div>" +
-                    " </div>";
-            } else {
-                document.getElementById("topEightSecondPage").innerHTML += "<div class=' card mx-1 my-4 ' style='max-width: 12rem; max-height:22rem;'>" +
-                    "<img class='card-img-top mx-auto my-auto' style='max-width:10rem; max-height:12rem;' src='img/intothestorm.jpg' alt='Card image cap'>" +
+                    "<img class='card-img-top mx-auto my-auto' style='max-width:10rem; max-height:12rem;' src='" + img + "' alt='Card image cap'>" +
                     "<div class='card-body'>" +
                     " <a href='#' class='text-decoration-none text-dark'>" +
                     "  <h5 class='card-title' id='title'>" + title + "</h5>" +
                     " </a>" +
                     "  <a href='#' id='genre' class='card-link text-muted text-decoration-none'>" + genre + "</a>" +
-                    " <a href='#' id='year' class='card-link text-muted text-decoration-none'>(2020)</a>" +
+                    " <a href='#' id='year' class='card-link text-muted text-decoration-none'>(" + year + ")</a>" +
+                    " </div>" +
+                    " </div>";
+            } else {
+                document.getElementById("topEightSecondPage").innerHTML += "<div class=' card mx-1 my-4 ' style='max-width: 12rem; max-height:22rem;'>" +
+                    "<img class='card-img-top mx-auto my-auto' style='max-width:10rem; max-height:12rem;' src='" + img + "' alt='Card image cap'>" +
+                    "<div class='card-body'>" +
+                    " <a href='#' class='text-decoration-none text-dark'>" +
+                    "  <h5 class='card-title' id='title'>" + title + "</h5>" +
+                    " </a>" +
+                    "  <a href='#' id='genre' class='card-link text-muted text-decoration-none'>" + genre + "</a>" +
+                    " <a href='#' id='year' class='card-link text-muted text-decoration-none'>(" + year + ")</a>" +
                     " </div>" +
                     " </div>";
 
@@ -68,9 +72,7 @@ function getListOfGenres() {
 
         for (let i = 0; i < obj.genres.length; i++) {
             genres.push(obj.genres[i]);
-
         }
-        console.log(obj.genres[1]);
         return genres;
 
 
@@ -82,10 +84,29 @@ function getListOfGenres() {
 
 }
 
-function getNameFromGenreId() {
+async function getNameFromGenreId(genreId) {
 
-    let genres = getListOfGenres();
+    type = "genre/movie/list";
+    let url = apiUrl + type + apiKey;
 
-    console.log();
+
+    fetch(url).then(response => response.json())
+        .then(function(obj) {
+
+            for (let i = 0; i < obj.genres.length; i++) {
+                if (obj.genres[i].id === genreId) {
+                    genreName = obj.genres[i].name;
+                    return genreName;
+
+                }
+
+            }
+
+
+
+        }).catch(function(error) {
+            console.error("Something went wrong");
+            console.error(error);
+        })
 
 }
